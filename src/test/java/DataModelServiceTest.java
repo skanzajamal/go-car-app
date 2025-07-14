3abfc0f28651959056f246ce68ae4f8906ada3d3
Kanza Jamal <skanzajamal@gmail.com>
2025-07-14 15:40:47 +1000

    add test cases

--- src/test/java/DataModelServiceTest.java
+++ src/test/java/DataModelServiceTest.java
@@ -2,15 +2,18 @@ import com.entity.DriverEntity;
 import com.repository.DriverRepository;
 import com.service.DataModelService;
 import com.specification.SearchRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Transactional  // <-- keeps session open
 public class DataModelServiceTest extends MainApplicationTest{
 
     @Autowired
@@ -22,16 +25,33 @@ public class DataModelServiceTest extends MainApplicationTest{
     @Test
     void testSearchByCriteria() {
 
-        // when status is online
+        // when status is online and longitude and latitude are not provided in search field
         SearchRequest criteria1 = new SearchRequest("ONLINE", null, null);
         List<DriverEntity> result1 = dataModelService.driverList(criteria1);
         assertEquals(6, result1.size());
 
-        // when status is online and longitude and latitude is not null
+        // when status is online and longitude and latitude are provided in search field
         SearchRequest criteria2 = new SearchRequest("ONLINE", 55.7558, 37.6173);
         List<DriverEntity> result2 = dataModelService.driverList(criteria2);
         assertEquals(1, result2.size());
         assertThat(result2.get(0).getUserName()).isEqualTo("driver05");
+    }
+
+    @Test
+    void testSelectAndDeSelectCar() throws Exception {
+
+        //select car
+        dataModelService.selectCar(6, 8);
+        var test = driverRepository.findById(6);
+
+        //check if the car has been selected
+        driverRepository.findById(8).ifPresent(record -> Assertions.assertThat(record.getCar().getId()).isEqualTo(6));
+
+        //deselect car
+        dataModelService.deselectCar(8, 6);
+
+        //check if the car has been deselected
+        driverRepository.findById(6).ifPresent(record -> Assertions.assertThat(record.getCar()).isNull());
     }
 
 } //ENDCLASS
